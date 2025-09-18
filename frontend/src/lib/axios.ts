b0506373b8002d3adcdd2334c6d77e01fbbefc70
BochraBenRomdhane <bochrabenromdhane2003@gmail.com>
2025-09-18 23:17:24 +0100

    test

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log(import.meta.env.VITE_API_URL);
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://messaging-system-n0wz.onrender.com/api"),
   withCredentials: false, // Always disable credentials since we're using localStorage
