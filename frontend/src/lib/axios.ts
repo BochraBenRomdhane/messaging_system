4fee9f2c6aa795bfddef312157fc965e4de7afef
BochraBenRomdhane <bochrabenromdhane2003@gmail.com>
2025-09-18 21:43:26 +0100

    fix touh=che

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://messaging-system-n0wz.onrender.com/api" : "/api",
+  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://messaging-system-n0wz.onrender.com/api"),
   withCredentials: true,
 });
